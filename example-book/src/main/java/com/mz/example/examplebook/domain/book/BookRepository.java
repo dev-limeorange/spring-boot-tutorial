f2e6a49a6a44a68492f0b5d353d026cf91979878
mz01-khjeong <dev.limeorange@gmail.com>
2020-02-11 09:39:10 +0900

    200211 리패키징

---
+++ example-book/src/main/java/com/mz/example/examplebook/domain/book/BookRepository.java
@@ -0,0 +1,12 @@
+package com.mz.example.examplebook.domain.book;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface BookRepository extends CrudRepository<BookEntity, UUID> {
+
+}
